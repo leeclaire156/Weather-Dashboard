b32b70e101e5794ed9fedc6a45b5661c3e479ec3
lee.claire156 <lee.claire156@gmail.com>
2022-12-14 01:23:15 -0500

    Adds current weather's icon

--- Develop/Assets/script.js
+++ Develop/Assets/script.js
@@ -11,19 +11,28 @@ function getCity() {
                 return response.json();
             })
             .then(function (data) {
-                console.log(data);
+                // console.log(data);
+
+                //City Name
                 var cityName = document.querySelector(".city-name");
+                //Displays Date
                 var dateDefault = Date(data.coord.dt);
                 var dateToString = new Date(dateDefault);
                 var dateToDisplayed = (dateToString.getMonth() + 1) + '/' + dateToString.getDate() + '/' + dateToString.getFullYear();
                 cityName.innerText = data.name + " " + dateToDisplayed;
-
-                //icon code here
-
+                //Icon code
+                var iconCode = data.weather[0].icon;
+                //Notes for the icon URL: @2x doubles the dimensions of the icon's image
+                var iconUrl = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+                var weatherIcon = document.querySelector(".weather-icon");
+                weatherIcon.setAttribute("src", iconUrl);
+                //Temp
                 var todayTemp = document.querySelector(".today-temp");
                 todayTemp.innerText = data.main.temp + " \u00B0" + "F";
+                //Wind Speeds
                 var todayWind = document.querySelector(".today-wind");
                 todayWind.innerText = data.wind.speed + " MPH";
+                //Humidity
                 var todayHumidity = document.querySelector(".today-humidity");
                 todayHumidity.innerText = data.main.humidity + "%";
             })
